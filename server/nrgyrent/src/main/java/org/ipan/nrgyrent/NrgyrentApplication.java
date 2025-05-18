ce7027f28474e3b9dda1e23e603250e4ab580a1e
ivan <ivanpaniotov@gmail.com>
2025-05-18 19:20:25 +0300

    encrypt private key for managed wallets

--- server/nrgyrent/src/main/java/org/ipan/nrgyrent/NrgyrentApplication.java
+++ server/nrgyrent/src/main/java/org/ipan/nrgyrent/NrgyrentApplication.java
@@ -1,9 +1,9 @@
 package org.ipan.nrgyrent;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class NrgyrentApplication {
--- server/nrgyrent/src/main/java/org/ipan/nrgyrent/domain/service/ManagedWalletService.java
+++ server/nrgyrent/src/main/java/org/ipan/nrgyrent/domain/service/ManagedWalletService.java
@@ -1,14 +1,33 @@
 package org.ipan.nrgyrent.domain.service;
 
 import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.ipan.nrgyrent.domain.model.ManagedWallet;
 import org.ipan.nrgyrent.tron.crypto.ECKey;
 import org.ipan.nrgyrent.tron.wallet.WalletApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import lombok.SneakyThrows;
 
 @Service
 public class ManagedWalletService {
+    private static final String ALGO = "AES/GCM/NoPadding";
+    private static final int GCM_TAG_LENGTH = 128;
+    private static final int IV_LENGTH = 12;
+
+    private final byte[] key;
+
+    public ManagedWalletService(@Value("${app.wallet.encryption.keyBase64}") String key) {
+        this.key = Base64.getDecoder().decode(key);
+    }
 
     // TODO: Encrypt the private key
     public ManagedWallet generateDepositWallet() throws IOException {
@@ -17,8 +36,39 @@ public class ManagedWalletService {
         ECKey privateKeyForNewWallet = WalletApi.generatePrivateKeyForNewWallet();
         byte[] address = privateKeyForNewWallet.getAddress();
         managedWallet.setBase58Address(WalletApi.encode58Check(address));
-        managedWallet.setPrivateKeyEncrypted(privateKeyForNewWallet.getPrivateKey());
+
+        byte[] privateKey = privateKeyForNewWallet.getPrivKeyBytes();
+        byte[] encryptedPrivateKey = encrypt(privateKey, key);
+        managedWallet.setPrivateKeyEncrypted(encryptedPrivateKey);
 
         return managedWallet;
+    }
+
+    @SneakyThrows
+    public static byte[] encrypt(byte[] plaintext, byte[] key) {
+        Cipher cipher = Cipher.getInstance(ALGO);
+        byte[] iv = new byte[IV_LENGTH];
+        new SecureRandom().nextBytes(iv);
+        GCMParameterSpec spec = new GCMParameterSpec(GCM_TAG_LENGTH, iv);
+        SecretKey secretKey = new SecretKeySpec(key, "AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, spec);
+        byte[] encrypted = cipher.doFinal(plaintext);
+
+        // prepend IV for storage
+        byte[] result = new byte[IV_LENGTH + encrypted.length];
+        System.arraycopy(iv, 0, result, 0, IV_LENGTH);
+        System.arraycopy(encrypted, 0, result, IV_LENGTH, encrypted.length);
+        return result;
+    }
+
+    @SneakyThrows
+    public static byte[] decrypt(byte[] ciphertext, byte[] key) {
+        byte[] iv = new byte[IV_LENGTH];
+        System.arraycopy(ciphertext, 0, iv, 0, IV_LENGTH);
+        GCMParameterSpec spec = new GCMParameterSpec(GCM_TAG_LENGTH, iv);
+        SecretKey secretKey = new SecretKeySpec(key, "AES");
+        Cipher cipher = Cipher.getInstance(ALGO);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, spec);
+        return cipher.doFinal(ciphertext, IV_LENGTH, ciphertext.length - IV_LENGTH);
     }
 }
