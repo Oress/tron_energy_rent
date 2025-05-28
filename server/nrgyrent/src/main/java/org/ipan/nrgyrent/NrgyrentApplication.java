a8ea0c540a8e9f8ab95a8f5749ca73e06ad81615
ivan <ivanpaniotov@gmail.com>
2025-05-28 14:21:35 +0300

    snippet for generating AES key

--- server/nrgyrent/src/main/java/org/ipan/nrgyrent/NrgyrentApplication.java
+++ server/nrgyrent/src/main/java/org/ipan/nrgyrent/NrgyrentApplication.java
@@ -1,6 +1,8 @@
 package org.ipan.nrgyrent;
 
 import java.io.IOException;
+import java.security.SecureRandom;
+import java.util.Base64;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NrgyrentApplication {
     public static void main(String[] args) throws IOException {
         SpringApplication.run(NrgyrentApplication.class, args);
+
+        // byte[] key = new byte[32]; // 32 bytes = 256 bits for AES-256
+        // new SecureRandom().nextBytes(key);
+        // String base64Key = Base64.getEncoder().encodeToString(key);
+        // System.out.println("Base64 AES key: " + base64Key);
     }
 }
