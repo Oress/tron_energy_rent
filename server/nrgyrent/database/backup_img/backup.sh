c019143a711a0602c24180f274125d2ae7f79ed5
ivan <ivanpaniotov@gmail.com>
2025-05-28 14:18:20 +0300

    backup part 3

--- server/nrgyrent/database/backup_img/backup.sh
+++ server/nrgyrent/database/backup_img/backup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 echo "Backup started"
-echo $(/usr/bin/pg_dump -w -h "$POSTGRES_DB_HOST" -U "$POSTGRES_USER" 2> /error.log) > /db_backup.sql
+echo $(/usr/bin/pg_dump -w -h "$POSTGRES_DB_HOST" -U "$POSTGRES_USER" -f /db_backup.sql 2> /error.log)
 cat /error.log
 echo "Backup finished. Uploading to B2..."
 /b2 file upload $B2_BUCKET_NAME /db_backup.sql $(date +"%Y-%m-%d-%H-%M-%S.backup") 2> /error1.log
 cat /error1.log
 echo "Upload finished."
+rm /db_backup.sql
+echo "Removing old backups..."
