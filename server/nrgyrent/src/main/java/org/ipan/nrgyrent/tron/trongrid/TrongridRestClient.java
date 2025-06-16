8400c40331f34264085ee3e6c2925f6ef1445913
ivan <ivanpaniotov@gmail.com>
2025-06-17 02:01:28 +0300

    decrease qps trongrid

--- server/nrgyrent/src/main/java/org/ipan/nrgyrent/tron/trongrid/TrongridRestClient.java
+++ server/nrgyrent/src/main/java/org/ipan/nrgyrent/tron/trongrid/TrongridRestClient.java
@@ -38,7 +38,7 @@ public class TrongridRestClient {
 
     public TrongridRestClient(
             ObjectMapper objectMapper,
-            @Value("${app.trongrid.qps:10}") Integer qps,
+            @Value("${app.trongrid.qps:14}") Integer qps,
             @Value("${app.trongrid.base-url:https://nile.trongrid.io}") String baseUrl,
             @Value("${app.trongrid.api-key}") String apiKey) {
         this.objectMapper = objectMapper;
